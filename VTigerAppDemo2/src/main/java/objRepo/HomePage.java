5f7a5f2f9ee536acca24930b73150e2fbc82a8f0
Ajith Chowdary <Ajith Chowdary@Vandhana-Ajith>
2026-09-21 20:19:34 +0530

    edited

--- VTigerAppDemo2/src/main/java/objRepo/HomePage.java
+++ VTigerAppDemo2/src/main/java/objRepo/HomePage.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.support.PageFactory;
 public class HomePage 
 {
 	WebDriver driver;
-	public HomePage(WebDriver driver)
+	public HomePage(WebDriver driver) 
 	{
 		this.driver=driver;
 		PageFactory.initElements(driver, this);
